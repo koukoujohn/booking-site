d22a6072195213959e01fabe0175b0aa8d903dce
koukoujohn <koukoujohn13@yahoo.gr>
2020-09-11 18:09:39 +0300

    add Header.js component and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+// import Home from './Home';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Booking site</h1>
+      <Header />
+      {/* <Home /> */}
       
       {/* Home */}  
           {/* Header */}
